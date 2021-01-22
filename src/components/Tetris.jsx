299bc69a13e6f6e4f9945bc74e7e9e88c78f0c14
modesty0310 <rlatprua4@gmail.com>
2021-01-22 16:38:57 +0900

    stage, player 컨트롤을 위한 custom hook 생성

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Stage from './Stage';
 import StartButton from './StartButton';
-import {createStage} from '../gameHelpers'
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
 
 const Tetris = () => {
+    const [dromTime,setDropTime] = useState(null);
+    const [gameOver,setGameOver] = useState(false);
 
+    const [player] = usePlayer();
+    const [stage,setStage] = useStage(player);
 
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()} />
+                <Stage stage={stage} />
                 <aside>
+                    {gameOver ? 
+                    <Display gameOver={gameOver} text="Game Over" /> :
+                    (
                     <div>
                         <Display text="Scroe" />
                         <Display text="Rows" />
                         <Display text="Level" />
                     </div>
+                    )}
                     <StartButton />
                 </aside>
             </StyledTetris>
